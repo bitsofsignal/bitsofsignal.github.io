b27e880c8e944d2d907aaec44c4b2abece947743
bitsofsignal <bloggerwho@gmail.com>
2015-05-18 19:33:47 -0700

    Add thumbnails everywhere

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,26 @@
 $('body').on('click.zoom', '.enable-zoom:not(.zoomed)', function(e) {
   var that = $(this)
+  var src = $(this).attr('src')
+  var zoomedSrc = $(this).attr('zoomed-src')
+
   that.addClass('zoomed')
+
+  if (zoomedSrc) {
+    that.attr('src', zoomedSrc)
+  }
+
   $('.widewrapper').addClass('zoom-backdrop')
   $('.panel').addClass('zoom-backdrop')
+
   $('body').one('click.zoom', function() {
     that.removeClass('zoomed')
+
     if ($('.zoomed').size() === 0) {
       $('.zoom-backdrop').removeClass('zoom-backdrop')
+
+      if (zoomedSrc) {
+        that.attr('src', src)
+      }
     }
   })
 })
